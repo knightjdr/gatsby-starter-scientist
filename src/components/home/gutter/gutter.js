1170ae477c2ac13183d160c759363d4afefd95b7
James Knight <knightjdr@gmail.com>
2020-06-07 15:27:19 -0400

    Add gutter background to home page

---
+++ src/components/home/gutter/gutter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './gutter.css';
+
+export const GutterLeft = () => (
+  <div className="gutter gutter_left" />
+);
+
+export const GutterRight = () => (
+  <div className="gutter gutter_right" />
+);
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,15 +3,20 @@ import React from 'react';
 import About from './about/about-container';
 import Landing from './landing/landing-container';
 import Research from './research/research-container';
+import { GutterLeft, GutterRight } from './gutter/gutter';
 
 import './home.css';
 
 const Home = () => (
   <>
     <Landing />
-    <div className="home__inner">
-      <About />
-      <Research />
+    <div className="home__layout">
+      <GutterLeft />
+      <div className="home__inner">
+        <About />
+        <Research />
+      </div>
+      <GutterRight />
     </div>
   </>
 );
